3d0b4617e6fd76e14a8c68ad28e64933b68fb5c0
Jan Thomä <janthomae@janthomae.de>
2014-11-16 06:54:31 +0100

    Check if the method is run at all.

--- Assets/CloudBuildIntegration/Editor/RunUT.cs
+++ Assets/CloudBuildIntegration/Editor/RunUT.cs
@@ -7,13 +7,13 @@ public class RunUT
     public static void RunPlan()
     {
         Debug.Log("About to run a plan.");
-        UTPreferences.DebugMode = true;
-        UTAutomationPlan thePlan = UTomate.UTAutomationPlanByName("Prepare");
-        if (thePlan == null)
-        {
-            throw new ArgumentException("There is no plan.");
-        }
+        /*   UTPreferences.DebugMode = true;
+           UTAutomationPlan thePlan = UTomate.UTAutomationPlanByName("Prepare");
+           if (thePlan == null)
+           {
+               throw new ArgumentException("There is no plan.");
+           }
 
-        UTomate.Run(thePlan);
+           UTomate.Run(thePlan);*/
     }
 }
