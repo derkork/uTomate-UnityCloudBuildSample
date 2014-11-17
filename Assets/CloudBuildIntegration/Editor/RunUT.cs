069617530bf26ec6a14ad46266b9a80692e0c49c
Jan Thomä <janthomae@janthomae.de>
2014-11-17 08:29:21 +0100

    Re-enabled utomate.

--- Assets/CloudBuildIntegration/Editor/RunUT.cs
+++ Assets/CloudBuildIntegration/Editor/RunUT.cs
@@ -7,13 +7,16 @@ public class RunUT
     public static void RunPlan()
     {
         Debug.LogWarning("About to run a plan.");
-        /*   UTPreferences.DebugMode = true;
-           UTAutomationPlan thePlan = UTomate.UTAutomationPlanByName("Prepare");
-           if (thePlan == null)
-           {
-               throw new ArgumentException("There is no plan.");
-           }
-
-           UTomate.Run(thePlan);*/
+        UTPreferences.DebugMode = true;
+        UTAutomationPlan thePlan = UTomate.UTAutomationPlanByName("Prepare");
+        if (thePlan == null)
+        {
+            throw new ArgumentException("There is no plan.");
+        }
+        var start = Time.realtimeSinceStartup;
+        Debug.LogWarning("Running the plan now.");
+        UTomate.Run(thePlan);
+        var took = Time.realtimeSinceStartup - start;
+        Debug.LogWarning("Plan ran. Took " + took + "ms");
     }
 }
