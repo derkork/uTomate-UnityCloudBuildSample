5683c0679a13ece8d394b9e3dc6c945c50a86408
Jan Thomä <janthomae@janthomae.de>
2014-11-18 21:26:10 +0100

    Manifest action prints more

--- Assets/CloudBuildIntegration/Editor/Demo Actions/UTPrintManifestInfoAction.cs
+++ Assets/CloudBuildIntegration/Editor/Demo Actions/UTPrintManifestInfoAction.cs
@@ -8,7 +8,16 @@ public class UTPrintManifestInfoAction : UTAction
     public override IEnumerator Execute(UTContext context)
     {
         var manifest = UnityCloud.API.CloudBuildAPI.GetBuildManifest();
-        Debug.Log("You are building " + manifest.ProjectId + " commit " + manifest.ScmCommitId + " on Unity " + manifest.UnityVersion);
+        Debug.Log("Manifest Info: \n" +
+            "Project: " + manifest.ProjectId + "\n" +
+            "Start Time: " + manifest.BuildStartTime + "\n" +
+            "SCM Commit-ID: " + manifest.ScmCommitId + "\n" +
+            "SCM Branch: " + manifest.ScmBranch + "\n" +
+            "Build Number: " + manifest.BuildNumber + "\n" +
+            "Bundle-ID: " + manifest.BundleId + "\n" +
+            "Target Name: " + manifest.CloudBuildTargetName + "\n" +
+            "Unity Version: " + manifest.UnityVersion + "\n" +
+            "XCode Version: " + manifest.XCodeVersion + "\n");
         yield break;
     }
 
